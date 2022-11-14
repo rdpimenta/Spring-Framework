d1e7c130852c82bca0528a1704ee1fd34acceafa
Roger Pimenta <roger.pimenta@live.com>
2022-11-14 15:04:25 -0300

    formulario de login

--- Java Servlet: Programação web java/src/br/com/alura/gerenciador/modelo/Banco.java
+++ Java Servlet: Programação web java/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -5,16 +5,30 @@ import java.util.List;
 
 public class Banco {
     private static final List<Empresa> lista = new ArrayList<>();
+    private static List<Usuario> listaUsuarios= new ArrayList<>();
 
     static {
         Empresa empresa = new Empresa();
         empresa.setNome("Alura");
         empresa.setId(1);
+
         Empresa empresa2 = new Empresa();
         empresa2.setNome("Caelum");
         empresa2.setId(2);
+
         lista.add(empresa);
         lista.add(empresa2);
+
+        Usuario u1 = new Usuario();
+        u1.setLogin("nico");
+        u1.setSenha("12345");
+
+        Usuario u2 = new Usuario();
+        u2.setLogin("ana");
+        u2.setSenha("12345");
+
+        listaUsuarios.add(u1);
+        listaUsuarios.add(u2);
     }
 
     public static List<Empresa> getLista() {
@@ -45,5 +59,14 @@ public class Banco {
 
     public List<Empresa> getEmpresas() {
         return lista;
+    }
+
+    public Usuario existeUsuario(String login, String senha) {
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.ehIgual(login, senha)) {
+                return usuario;
+            }
+        }
+        return null;
     }
 }
