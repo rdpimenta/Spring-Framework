099acdb201a52013fda245bb927aa73f57c72e1c
Roger Pimenta <roger.pimenta@live.com>
2022-11-17 10:25:07 -0300

    removendo projeto antigo

--- Java Servlet: Programação web java/src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.gerenciador.acao;
-
-import javax.servlet.http.HttpServletRequest;
-
-public class NovaEmpresaForm implements Acao {
-    public String executa(HttpServletRequest request) {
-        return "forward:formNovaEmpresa.jsp";
-    }
-}
