3aa3b0625e18d39c172ab5678ed121fb243af43c
Roger Pimenta <roger.pimenta@live.com>
2022-11-21 17:36:24 -0300

    controle de transação

--- JDBC/loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ JDBC/loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -1,26 +1,45 @@
 import java.sql.*;
+import java.util.Objects;
 
 public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
-        String nome = "Mouse";
-        String descricao = "Mouse sem fio";
-
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection connection = connectionFactory.recuperarConexao();
+        try (Connection connection = connectionFactory.recuperarConexao()) {
+            connection.setAutoCommit(false);
 
-        PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+            try (
+                    PreparedStatement statement = connection.prepareStatement(
+                            "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+                            Statement.RETURN_GENERATED_KEYS
+                    )
+            ) {
+                adicionarVariavel("Mouse", "Mouse sem fio", statement);
+                adicionarVariavel("Radio", "Radio de bateria", statement);
 
+                connection.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("ROLLBACK");
+                connection.rollback();
+            }
+        }
+    }
+
+    private static void adicionarVariavel(String nome, String descricao, PreparedStatement statement) throws SQLException {
         statement.setString(1, nome);
         statement.setString(2, descricao);
 
         statement.execute();
 
-        ResultSet resultSet = statement.getGeneratedKeys();
-        while (resultSet.next()) {
-            int id = resultSet.getInt(1);
-            System.out.println("O id criado foi: " + id);
-        }
+//        if (Objects.equals(nome, "Radio")) {
+//            throw new SQLException("Exceção executada");
+//        }
 
-        connection.close();
+        try (ResultSet resultSet = statement.getGeneratedKeys()) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                System.out.println("O id criado foi: " + id);
+            }
+        }
     }
 }
