b37dafd63ada4d7db1241131a9237ca4611085f4
Roger Pimenta <roger.pimenta@live.com>
2022-11-25 10:35:44 -0300

    ciclo de vida de uma entidade

--- Persistência com JPA-Hibernate/loja/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
+++ Persistência com JPA-Hibernate/loja/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
@@ -15,4 +15,13 @@ public class CategoriaDAO {
     public void cadastrar(Categoria categoria) {
         this.entityManager.persist(categoria);
     }
+
+    public void atualizar(Categoria categoria) {
+        this.entityManager.merge(categoria);
+    }
+
+    public void remover(Categoria categoria) {
+        categoria = entityManager.merge(categoria);
+        this.entityManager.remove(categoria);
+    }
 }
