6f24c42d01ee41a888c8190f48e0f6711be12c9b
Roger Pimenta <roger.pimenta@live.com>
2022-12-02 12:01:41 -0300

    atualização e deleção de médicos

--- Spring boot 3/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring boot 3/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -37,7 +34,28 @@ public class MedicoController {
             Pageable paginacao
     ) {
         return medicoRepository
-                .findAll(paginacao)
+                .findAllByAtivoTrue(paginacao)
                 .map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(
+            @RequestBody
+            @Valid
+            DadosAtualizacaoMedico dados
+    ) {
+        Medico medico = medicoRepository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void atualizar(
+            @PathVariable
+            Long id
+    ) {
+        var medico = medicoRepository.getReferenceById(id);
+        medico.excluir();
     }
 }
